12cb02742efa7721dc5e1a78715efdc64b90369f
David Way <david.way12@gmail.com>
2013-11-14 00:21:32 -0500

    got initial blurring done

---
+++ BlendImages.m
@@ -0,0 +1,24 @@
+function [im] = BlendImages(im1, im2, mask)
+
+
+[pyrAR,pindAR] = buildLpyr(im1(:,:,1), 4);
+[pyrAG,pindAG] = buildLpyr(im1(:,:,2), 4);
+[pyrAB,pindAB] = buildLpyr(im1(:,:,3), 4);
+[pyrBR,pindBR] = buildLpyr(im2(:,:,1), 4);
+[pyrBG,pindBG] = buildLpyr(im2(:,:,2), 4);
+[pyrBB,pindBB] = buildLpyr(im2(:,:,3), 4);
+[pyrG,pindG] = buildGpyr(mask, 4, fspecial('gaussian', [1 16], 4));
+pyrLR = pyrG .* pyrAR + (1 - pyrG) .* pyrBR;
+pyrLG = pyrG .* pyrAG + (1 - pyrG) .* pyrBG;
+pyrLB = pyrG .* pyrAB + (1 - pyrG) .* pyrBB;
+imR = reconLpyr(pyrLR, pindG);
+imG = reconLpyr(pyrLG, pindG);
+imB = reconLpyr(pyrLB, pindG);
+im1(:,:,1) = imR;
+im1(:,:,2) = imG;
+im1(:,:,3) = imB;
+im = im1;
+figure;
+imshow(im);
+
+end
--- KLTTracker.m
+++ KLTTracker.m
@@ -1,11 +1,20 @@
-
+close all;
 %% Initializes Necessary Components 
-videoFileReader = vision.VideoFileReader('apple.mp4');
-videoPlayer = vision.VideoPlayer('Position', [100, 100, 680, 520]);
+% APPLE IS READER 1
+videoFileReader1 = vision.VideoFileReader('apple.mp4');
+%videoPlayer1 = vision.VideoPlayer('Position', [100, 100, 680, 520]);
 
-objectFrame = step(videoFileReader);
-figure; imshow(objectFrame);
-objectRegion=round(getPosition(imrect))
+% ORANGE IS READER 2
+videoFileReader2 = vision.VideoFileReader('test.mp4');
+%videoPlayer2 = vision.VideoPlayer('Position', [100, 100, 680, 520]);
+
+objectFrame1 = step(videoFileReader1);
+figure; imshow(objectFrame1);
+objectRegion1 = round(getPosition(imrect))
+
+objectFrame2 = step(videoFileReader2);
+figure; imshow(objectFrame2);
+objectRegion2 = round(getPosition(imrect))
 
 
 %% Shows the Bounding Box 
@@ -13,23 +22,78 @@ objectRegion=round(getPosition(imrect))
 %figure; imshow(objectImage); title('Yellow box shows object region');
 
 %% Get Points of Interest and Mark Them
-points = detectMinEigenFeatures(rgb2gray(objectFrame), 'ROI', objectRegion);
-pointImage = insertMarker(objectFrame, points.Location, '+', 'Color', 'white');
-figure, imshow(pointImage), title('Detected interest points');
+points1 = detectMinEigenFeatures(rgb2gray(objectFrame1), 'ROI', objectRegion1);
+pointImage1 = insertMarker(objectFrame1, points1.Location, '+', 'Color', 'white');
+figure, imshow(pointImage1), title('Detected interest points');
 
-markerInserter = vision.MarkerInserter('Shape','Plus','BorderColor','White');
-tracker = vision.PointTracker('MaxBidirectionalError', 1);
-initialize(tracker, points.Location, objectFrame);
+markerInserter1 = vision.MarkerInserter('Shape','Plus','BorderColor','White');
+tracker1 = vision.PointTracker('MaxBidirectionalError', 1);
+initialize(tracker1, points1.Location, objectFrame1);
+
+points2 = detectMinEigenFeatures(rgb2gray(objectFrame2), 'ROI', objectRegion2);
+pointImage2 = insertMarker(objectFrame2, points2.Location, '+', 'Color', 'white');
+figure, imshow(pointImage2), title('Detected interest points');
+
+markerInserter2 = vision.MarkerInserter('Shape','Plus','BorderColor','White');
+tracker2 = vision.PointTracker('MaxBidirectionalError', 1);
+initialize(tracker2, points2.Location, objectFrame2);
 
 %% Track the Object
-while ~isDone(videoFileReader)
-      frame = step(videoFileReader);
-      [points, validity] = step(tracker, frame);
-      out = insertMarker(frame, points(validity, :), '+');
+%while ~isDone(videoFileReader)
+for x = 1:1
+      frame1 = step(videoFileReader1);
+      [points1, validity1] = step(tracker1, frame1);
+      out1 = insertMarker(frame1, points1(validity1, :), '+');
       % you can comment out this line if you don't want the videoplayer
-      step(videoPlayer, out);
+      %step(videoPlayer1, out1);
+      
+      frame2 = step(videoFileReader2);
+      [points2, validity2] = step(tracker2, frame2);
+      out2 = insertMarker(frame2, points2(validity2, :), '+');
+      % you can comment out this line if you don't want the videoplayer
+      %step(videoPlayer2, out2);
+      
+      %{
+      get bounding boxes
+      extract larger box from right image
+      make new image with larger box in the correct position
+      make mask
+      blur images
+      %}
+      vid1_x1 = round(min(points1(:,1)))
+      vid1_x2 = round(max(points1(:,1)))
+      
+      vid1_y1 = round(min(points1(:,2)))
+      vid1_y2 = round(max(points1(:,2)))
+      
+      
+      vid2_x1 = round(min(points2(:,1)))
+      vid2_x2 = round(max(points2(:,1)))
+      vid2_mid = round((vid2_x1 + vid2_x2) / 2)
+      vid2_y1 = round(min(points2(:,2)))
+      vid2_y2 = round(max(points2(:,2)))
+      
+      offset = 50;
+      vid2_x1 = max(1, vid2_mid - offset)
+      x_align = vid1_x2 - (vid2_x2 - vid2_x1)
+      vid2_x2 = min(size(frame2, 2), vid2_x2 + offset)
+      vid2_y1 = max(1, vid2_y1 - offset)
+      y_align = vid1_y1 - offset
+      vid2_y2 = min(size(frame2, 1), vid2_y2 + offset)
+      extraction = frame2( vid2_y1:vid2_y2,vid2_x1:vid2_x2,:);
+      
+      temp_im = zeros(size(frame1));
+      temp_im( y_align:y_align+size(extraction,1)-1,x_align:x_align+size(extraction,2)-1, :) = extraction;
+      
+      mask = ones(size(frame1,1),size(frame1,2));
+      mask( y_align+offset:y_align+size(extraction,1)-offset,x_align+offset:x_align+size(extraction,2)-offset) = 0;
+      BlendImages(im2double(frame1),temp_im,mask);
+      
+      
 end
 
 %% Clean Up Objects
-release(videoPlayer);
-release(videoFileReader);
+%release(videoPlayer1);
+release(videoFileReader1);
+%release(videoPlayer2);
+release(videoFileReader2);
