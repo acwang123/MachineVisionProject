ee356a120c262abe4d19b60b9c878c082e29ee9c
Jackie Chen <jchen527@mit.edu>
2013-11-21 18:30:01 -0500

    added version that blends the first video with only first frame of second video

--- BlendVideoPicture.m
+++ BlendVideoPicture.m
@@ -1,13 +1,10 @@
-close all;
 %% Initializes Necessary Components 
 
-videoOutput = VideoWriter('wipeTransition');
+videoOutput = VideoWriter('blendedVideoPicture');
 open(videoOutput);
 
 % APPLE IS READER 1
 videoFileReader1 = vision.VideoFileReader('apple.mp4');
-vidObj = VideoReader('apple.mp4');
-numFrames = vidObj.NumberOfFrames;
 % ORANGE IS READER 2
 videoFileReader2 = vision.VideoFileReader('test.mp4');
 
@@ -35,79 +32,56 @@ points2 = points2.Location;
 pointImage2 = insertMarker(frame2, points2, '+', 'Color', 'white');
 figure, imshow(pointImage2), title('Detected interest points');
 
-%tracker2 = vision.PointTracker('MaxBidirectionalError', 1);
-%initialize(tracker2, points2.Location, objectFrame2);
-
 %% Track the Object
-increment = -1;
-counter = 0;
-cutPosition = 0;
 offset = 20;
 vid2_x1 = round(min(points2(:,1)));
 vid2_x2 = round(max(points2(:,1)));
+vid2_mid = round((vid2_x1 + vid2_x2) / 2);
 vid2_y1 = round(min(points2(:,2)));
 vid2_y2 = round(max(points2(:,2)));
       
 % make sure the extraction with offset is within bounds
-vid2_x1 = max(1, vid2_x1 - offset);
+vid2_x1 = max(1, vid2_mid - offset);
 vid2_x2 = min(size(frame2, 2), vid2_x2 + offset);
 vid2_y1 = max(1, vid2_y1 - offset);
 vid2_y2 = min(size(frame2, 1), vid2_y2 + offset);
       
-while ~isDone(videoFileReader1)% && ~isDone(videoFileReader2)
-%for x = 1:1
-
-      counter = counter + 1;
-      if counter == increment
-          counter = 0;
-          cutPosition = min(cutPosition + 1, vid1_x2 - vid1_x1);
-      end
+while ~isDone(videoFileReader1)
 
       frame1 = step(videoFileReader1);
       frame1 = imresize(frame1,.4);
       [points1, validity1] = step(tracker1, frame1);
       points1 = points1(validity1,:);
       
-      %frame2 = step(videoFileReader2);
-      %frame2 = imresize(frame2, .4);
-      %[points2, validity2] = step(tracker2, frame2);
-      %points2 = points2(validity2,:);
-      
       % video 1 bounding box
       vid1_x1 = round(min(points1(:,1)));
       vid1_x2 = round(max(points1(:,1)));
+      vid1_mid = round((vid1_x1 + vid1_x2) / 2);
       vid1_y1 = round(min(points1(:,2)));
-      %vid1_y2 = round(max(points1(:,2)));
+      vid1_y2 = round(max(points1(:,2)));
       
-      if increment == -1
-          increment = floor(numFrames / (vid1_x2 - vid1_x1));
-      end
-
-      % scale the extraction to the same width as the blending object
+      % scale the extraction to the same height as the blending object
       extraction = frame2(vid2_y1:vid2_y2, vid2_x1:vid2_x2, :);
-      scale = (vid1_x2 - vid1_x1 + 2 * offset) / (vid2_x2 - vid2_x1);
+      scale = (vid1_y2 - vid1_y1 + 2 * offset) / (vid2_y2 - vid2_y1);
       extraction = imresize(extraction, scale);
 
       % temp_im is the extraction placed in the right frame position
       temp_im = zeros(size(frame1));
-      x_align = vid1_x1 - offset;
+      x_align = vid1_mid - offset;
       y_align = vid1_y1 - offset;
       temp_im(y_align:y_align+size(extraction,1)-1, x_align:x_align+size(extraction,2)-1, :) = extraction;
       
       % create mask with a different cut at each iteration
       mask = ones(size(frame1,1),size(frame1,2));
-      objYSize = round(size(extraction, 1) - 2 * offset * scale);
-      mask(vid1_y1:vid1_y1+objYSize, vid1_x1:vid1_x1+cutPosition) = 0;
+      objXSize = round(size(extraction, 2) - 2 * offset * scale);
+      mask(vid1_y1:vid1_y2, vid1_mid:vid1_mid+objXSize) = 0;
       
       blended_im = BlendImages(im2double(frame1),temp_im,mask);
       blended_im(blended_im > 1) = 1;
-      %figure; imshow(blended_im);
       writeVideo(videoOutput, blended_im);
       
 end
-close(videoOutput);
 %% Clean Up Objects
-%release(videoPlayer1);
+close(videoOutput);
 release(videoFileReader1);
-%release(videoPlayer2);
 release(videoFileReader2);
