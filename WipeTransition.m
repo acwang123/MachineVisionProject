c2e406d4692ee6eafd4d5a68e569bd6c9ac12227
Jackie Chen <jchen527@mit.edu>
2013-11-21 17:37:20 -0500

    fixed transition speed

--- WipeTransition.m
+++ WipeTransition.m
@@ -22,7 +22,6 @@ frame2 = im2double(frame2);
 figure; imshow(frame2);
 objectRegion2 = round(getPosition(imrect));
 
-
 %% Get Points of Interest and Mark Them
 points1 = detectMinEigenFeatures(rgb2gray(objectFrame1), 'ROI', objectRegion1);
 pointImage1 = insertMarker(objectFrame1, points1.Location, '+', 'Color', 'white');
@@ -40,8 +39,8 @@ figure, imshow(pointImage2), title('Detected interest points');
 %initialize(tracker2, points2.Location, objectFrame2);
 
 %% Track the Object
-%increment = -1;
-%counter = 0;
+increment = -1;
+counter = 0;
 cutPosition = 0;
 offset = 20;
 vid2_x1 = round(min(points2(:,1)));
@@ -58,13 +57,13 @@ vid2_y2 = min(size(frame2, 1), vid2_y2 + offset);
       
 while ~isDone(videoFileReader1)% && ~isDone(videoFileReader2)
 %for x = 1:1
-%{
+
       counter = counter + 1;
       if counter == increment
           counter = 0;
           cutPosition = min(cutPosition + 1, vid1_x2 - vid1_x1);
       end
-%}
+
       frame1 = step(videoFileReader1);
       frame1 = imresize(frame1,.4);
       [points1, validity1] = step(tracker1, frame1);
@@ -75,27 +74,14 @@ while ~isDone(videoFileReader1)% && ~isDone(videoFileReader2)
       %[points2, validity2] = step(tracker2, frame2);
       %points2 = points2(validity2,:);
       
-      %{
-      get bounding boxes
-      extract larger box from right image
-      make new image with larger box in the correct position
-      make mask
-      blur images
-      %}
       % video 1 bounding box
       vid1_x1 = round(min(points1(:,1)));
       vid1_x2 = round(max(points1(:,1)));
       vid1_y1 = round(min(points1(:,2)));
       %vid1_y2 = round(max(points1(:,2)));
       
-      %{
       if increment == -1
           increment = floor(numFrames / (vid1_x2 - vid1_x1));
-      end
-      %}
-      % cutPosition is the cut of the sliding mask
-      if cutPosition < (vid1_x2 - vid1_x1)
-          cutPosition = cutPosition + 1;
       end
 
       % scale the extraction to the same width as the blending object
@@ -115,6 +101,7 @@ while ~isDone(videoFileReader1)% && ~isDone(videoFileReader2)
       mask(vid1_y1:vid1_y1+objYSize, vid1_x1:vid1_x1+cutPosition) = 0;
       
       blended_im = BlendImages(im2double(frame1),temp_im,mask);
+      blended_im(blended_im > 1) = 1;
       %figure; imshow(blended_im);
       writeVideo(videoOutput, blended_im);
       
